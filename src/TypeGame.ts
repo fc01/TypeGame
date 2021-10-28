154bde67d651e3085d58f0e2d2421d07dbceb113
fc01 <910677221@qq.com>
2021-10-28 11:04:54 +0800

    添加注释

--- src/TypeGame.ts
+++ src/TypeGame.ts
@@ -1,16 +1,39 @@
 import { stages } from './stages'
 import { Index, Add1, Sub2, Sub1, Add2, Join, Render, MatrixRotateAndScale } from './utils'
 
+/*
+单个元素 下一步
+相对方向  主角前进的方向
+current 当前元素
+prev1 后面1个元素
+prev2 后面2个元素
+next1 前面1个元素
+next2 前面2个元素
+*/
 type Step<prev2, prev1, current, next1, next2> = Index<{
+    //地板
     '🌑': next1 extends ('👾' | '👽') ? '👾' : next1 extends ('😃' | '😎') ? (next2 extends ('👾' | '👽') ? '😃' : current) : current
+
+    //目标
     '🕶️': next1 extends ('👾' | '👽') ? '👽' : next1 extends ('😃' | '😎') ? (next2 extends ('👾' | '👽') ? '😎' : current) : current
+
+    //主角
     '👾': prev1 extends ('🌑' | '🕶️') ? '🌑' : prev1 extends ('😃' | '😎') ? (prev2 extends ('🌑' | '🕶️') ? '🌑' : current) : current
+
+    //主角+目标
     '👽': prev1 extends ('🌑' | '🕶️') ? '🕶️' : prev1 extends ('😃' | '😎') ? (prev2 extends ('🌑' | '🕶️') ? '🕶️' : current) : current
+
+    //砖块
     '🎇': '🎇'
+
+    //箱子
     '😃': next1 extends ('👾' | '👽') ? (prev1 extends ('🌑' | '🕶️') ? '👾' : current) : current
+
+    //箱子+目标
     '😎': next1 extends ('👾' | '👽') ? (prev1 extends ('🌑' | '🕶️') ? '👽' : current) : current
 }, current>
 
+//上下左右
 type Up<T> = MatrixRotateAndScale<Left<MatrixRotateAndScale<T>>>
 type Down<T> = MatrixRotateAndScale<Right<MatrixRotateAndScale<T>>>
 
@@ -20,6 +43,7 @@ type Left<T> = { [K in keyof T]: _Left<T[K]> }
 type _Right<T> = { [K in keyof T]: Step<Index<T, Add2<K>, ''>, Index<T, Add1<K>, ''>, T[K], Index<T, Sub1<K>, ''>, Index<T, Sub2<K>, ''>> }
 type Right<T> = { [K in keyof T]: _Right<T[K]> }
 
+//连招
 type Combo<T> = {
     w: Combo<Up<T>>
     s: Combo<Down<T>>
@@ -28,6 +52,7 @@ type Combo<T> = {
     over: GameRender<T>
 }
 
+//渲染
 type GameRender<T> = Render<{
     [K in keyof T]: Join<T[K]>
 }, [Combo<T>,
--- src/utils.ts
+++ src/utils.ts
@@ -1,28 +1,57 @@
+//类型里面就不再写类型了
+//直接各种暴力操作 强转
 export type Cast<A, B> = A extends B ? A : B
 export type Index<T, K, V = never> = K extends keyof T ? T[K] : V
 export type Length<T> = Cast<Index<T, 'length', 0>, number>
 
-/* ?????  TypeScript 4.4.2
-type A = ['🌑', '👾']
-type B = A[-1] //-------------> "🌑" | "👾"
-type C = -1 extends keyof A ? 1 : 0 //-------------> 1
-*/
-type Sub1Table = ['!!!!', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+//TypeScript 4.4.2
+//type A = ['a', 'b'][-1]  
+//返回的 'a' | 'b'
+type Sub1Table = ['这里就不用 -1 了', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 type Add1Table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+
+//20以内 +1 -1 就够用了 直接查表
 export type Sub1<T> = Index<Sub1Table, T>
 export type Add1<T> = Index<Add1Table, T>
 export type Sub2<T> = Sub1<Sub1<T>>
 export type Add2<T> = Add1<Add1<T>>
 
-export type MatrixRow<T, I> = { [K in keyof T]: Index<T[K], I> }
+/*
+矩阵 取1列
+MatrixCol<[
+    [1, 2, 3],
+    [4, 5, 6]
+], 0>
+返回 
+[1, 4]
+*/
+export type MatrixCol<T, I> = { [K in keyof T]: Index<T[K], I> }
+
+
+/*
+矩阵 旋转 + 翻转
+MatrixRotateAndScale<[
+    [1, 2, 3],
+    [4, 5, 6],
+]>
+返回 
+[
+    [1, 4],
+    [2, 5],
+    [3, 6],
+]
+*/
 export type MatrixRotateAndScale<T, N = 0> =
     N extends Length<Index<T, 0>> ? []
-    : [MatrixRow<T, N>, ...MatrixRotateAndScale<T, Add1<N>>]
+    : [MatrixCol<T, N>, ...MatrixRotateAndScale<T, Add1<N>>]
 
+
+//连接 ['a', 'b', 'c', 'd']  转成  'abcd'
 export type Join<T, N = 0> =
     N extends Length<T> ? ''
     : `${Cast<Index<T, N>, string>}${Join<T, Add1<N>>}`
 
+//渲染 ['a', 'b', 'c', 'd'] next  转成  { a: { b: { c: { d: next } } } }
 export type Render<T, next, N = 0> =
     N extends Length<T> ? next
     : { [K in Cast<Index<T, N>, string>]: Render<T, next, Add1<N>> }
